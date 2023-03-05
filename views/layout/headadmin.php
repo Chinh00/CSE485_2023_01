cef3a52bf83aad457765aca25913f6ff91d4b726
Giang Nguyễn Đức <nhokcunpvp@gmail.com>
2023-03-05 16:36:02 +0700

    MVC

--- views/layout/headadmin.php
+++ views/layout/headadmin.php
@@ -10,7 +10,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link active fw-bold" aria-current="page" href="./">Trang chủ</a>
+                        <a class="nav-link active fw-bold" aria-current="page" href="../../../btth01../">Trang chủ</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="../../../btth01/index.php">Trang ngoài</a>
